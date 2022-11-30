32300215e8ffa6e5dd2e6115d93b42864989bb4a
Kristof Majkut <k.majkut@student.utwente.nl>
2022-11-30 14:33:58 +0100

    use bigint for transfer ids

--- 02_pwn/common/Students_Deterlab_NS/secure_db.sql
+++ 02_pwn/common/Students_Deterlab_NS/secure_db.sql
@@ -2,3 +2,6 @@ USE ctf2;
 
 -- Add a timestamp for each transaction
 ALTER TABLE transfers ADD COLUMN timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
+
+-- Use BIGINT so that more transactions can be stored on the server
+ALTER TABLE transfers MODIFY id BIGINT NOT NULL auto_increment;
